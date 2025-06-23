91a29f59ad5d9940b2160253b051dad532d47d59
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-06-23 17:51:38 +0700

    feat: add carousel promo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import PromoCarousel from "@/components/PromoCarousel";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <PromoCarousel />
+
+      <div className="container mx-auto px-4 py-8"></div>
+    </div>
+  );
 };
 
 export default Home;
